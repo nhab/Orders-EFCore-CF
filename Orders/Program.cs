d6eb1f5421b8024d462f7d4159f87b629d7319a3
nhab <n.habibollahi@yahoo.com>
2022-08-07 10:21:00 +0430

    ! record Inserted into order table

--- Orders/Program.cs
+++ Orders/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Orders.EF;
+using Orders.Models;
+
+using (var ctx = new OrderDbContext())
+{
+    var order = new Order{ OrderDate=DateTime.Now };
+    ctx.Orders.Add(order);
+   
+    ctx.SaveChanges();
+}
+Console.WriteLine("1 order inserted!");
