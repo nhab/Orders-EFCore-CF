6475c61855e91ddbf32c3a43aa4b83ef48450ed3
nhab <n.habibollahi@yahoo.com>
2022-08-07 10:40:38 +0430

    fluentAPI config for table  relations and columns names and  indexes

--- Orders/EF/OrderDbContext.cs
+++ Orders/EF/OrderDbContext.cs
@@ -23,6 +23,34 @@ namespace Orders.EF
             modelBuilder.Entity<Order>()
                 .Property(b => b.OrderDate)
                 .HasDefaultValueSql("CONVERT(date, GETDATE())");
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.HasKey(e => e.OrderDetailID);
+
+                entity.HasIndex(e => e.OrderID);
+
+                entity.Property(e => e.OrderDetailID).HasColumnName("OrderDetailID");
+
+                entity.Property(e => e.OrderID).HasColumnName("OrderID");
+
+                entity.Property(e => e.ProductID).HasColumnName("ProductID");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderID);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.OrderID);
+
+                entity.Property(e => e.OrderID).HasColumnName("OrderID");
+
+                entity.Property(e => e.CustomerID).HasColumnName("CustomerID");
+
+                entity.Property(e => e.EmployeeID).HasColumnName("EmployeeID");
+            });
         }
     }
 }
--- Orders/Migrations/20220807060940_FluentAPIRelations.Designer.cs
+++ Orders/Migrations/20220807060940_FluentAPIRelations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Orders.EF;
 
@@ -11,9 +12,10 @@ using Orders.EF;
 namespace Orders.Migrations
 {
     [DbContext(typeof(OrderDbContext))]
-    partial class OrderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220807060940_FluentAPIRelations")]
+    partial class FluentAPIRelations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
