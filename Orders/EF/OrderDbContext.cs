201682789a4c915a7ca6a3d05af34a8f2ef86b2e
nhab <n.habibollahi@yahoo.com>
2022-08-07 10:06:29 +0430

    Migration added using Add-migraton Initial and update-database. Now Tables ar added to the database

---
+++ Orders/EF/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.EF
+{
+    public  class OrderDbContext:DbContext
+    {
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-CLSBCU4\N;Initial Catalog=StoreDB;persist security info=True; Integrated Security=SSPI;");
+        }
+    }
+}
